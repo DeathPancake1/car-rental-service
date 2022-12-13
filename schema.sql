3931e9f5524d0423b61676219422952c9bd2701c
amgedelmasry <85349889+amgedelmasry@users.noreply.github.com>
2022-12-13 02:40:34 +0200

    "Update schema.sql"
    DML new

--- schema.sql
+++ schema.sql
@@ -10,11 +10,28 @@ CREATE TABLE car(
     PRIMARY KEY (plate_id)
 );
 
-CREATE TABLE user(
+CREATE TABLE `user`(
     user_id int NOT NULL auto_increment,
     email varchar(100),
     `password` varchar(100),
     birthdate date,
     licence varchar(100),
     PRIMARY KEY (user_id)
+);
+CREATE table admin(
+    id int(15),
+	name varchar(15),
+    username varchar(15),
+    `password` varchar(15),
+    PRIMARY KEY (id)
+);
+CREATE TABLE reservation(
+	reservedate date,
+    pickupdate date,
+    returndate date,
+    user_id int NOT null AUTO_INCREMENT,
+    plate_id varchar(50) NOT null,
+    PRIMARY KEY(user_id,plate_id),
+    FOREIGN KEY (plate_id)  REFERENCES car(plate_id),
+    FOREIGN KEY (user_id )REFERENCES user(user_id)
 );
