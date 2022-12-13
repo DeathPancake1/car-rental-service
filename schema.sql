262eb32039d63dc54abe8673aeb6815606873131
DeathPancake1 <youssefyoyododo@gmail.com>
2022-12-13 02:30:38 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,20 @@
 CREATE SCHEMA car_rental;
 use car_rental;
+
+CREATE TABLE car(
+    plate_id int NOT NULL,
+    model varchar(100),
+    `year` YEAR,
+    `status` varchar(50),
+    price float,
+    PRIMARY KEY (plate_id)
+);
+
+CREATE TABLE user(
+    user_id int NOT NULL auto_increment,
+    email varchar(100),
+    `password` varchar(100),
+    birthdate date,
+    licence varchar(100),
+    PRIMARY KEY (user_id)
+);
