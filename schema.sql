68c34747e5171ac1f658e335699790945594ed44
DeathPancake1 <youssefyoyododo@gmail.com>
2022-12-18 00:01:04 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,9 +5,16 @@ CREATE TABLE car(
     plate_id varchar(50) NOT NULL,
     model varchar(100),
     `year` YEAR,
-    `status` varchar(50),
     price float,
     PRIMARY KEY (plate_id)
+);
+
+CREATE TABLE car_status(
+    plate_id varchar(50) NOT NULL,
+    today date,
+    `status` varchar(50),
+    reserved varchar(50),
+    PRIMARY KEY (plate_id,today)
 );
 
 CREATE TABLE `user`(
@@ -26,10 +33,12 @@ CREATE table admin(
     PRIMARY KEY (id)
 );
 CREATE TABLE reservation(
-	reservedate date,
-    pickupdate date,
-    returndate date,
+	reserve_date date,
+    pickup_date date,
+    return_date date,
     user_id int NOT null AUTO_INCREMENT,
+    reservation_number varchar(100) unique,
+    payment varchar(50),
     plate_id varchar(50) NOT null,
     PRIMARY KEY(user_id,plate_id),
     FOREIGN KEY (plate_id)  REFERENCES car(plate_id),
