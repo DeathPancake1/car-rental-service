99ef87cb9a1978950507cc10971c56a3a8d1e87d
DeathPancake1 <youssefyoyododo@gmail.com>
2022-12-26 15:16:09 +0200

    Fixed html for users and reservations

--- pages/functions.php
+++ pages/functions.php
@@ -35,30 +35,117 @@
     function show_users($res){
         $html_res = "";
         while($row = mysqli_fetch_array($res)) {
-            $delete ="<br><input type='button' value='Delete' onclick='deleteUser(event)'>";
-            $html_res .= "<div id=".$row['user_id']." class='user_div'><label class='user_name'>".$row['name'] .'</label>' 
-            ."<label class='use_email'>".$row['email'] .'</label>' .$delete."</div><br>";
+            $delete ="<br><input type='button' value='Delete' class='user_action' onclick='deleteUser(event)'>";
+            $html_res .= "<div id=".$row['user_id']." class='user_div'>".
+                            "<div>".
+                                "<label class='user_label'>"."User Name" .'</label><br>'.
+                                "<label class='user_name'>".$row['name'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='user_label'>"."Email" .'</label><br>'.
+                                "<label class='user_email'>".$row['email'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='user_label'>"."User ID" .'</label><br>'.
+                                "<label class='user_id'>".$row['user_id'] .'</label>'.
+                            "</div>".
+                $delete."</div><br>";
         }
         return $html_res;
     }
     function show_reservations($res){
         $html_res = "";
         while($row = mysqli_fetch_array($res)) {
-            $delete ="<br><input type='button' value='Delete' onclick='deleteReservation(event)'>";
-            $html_res .= "<div id=".$row['reservation_number']." class='reservation_div'><label class='user_name'>".$row['name'] .'</label>' 
-            ."<label class='use_email'>".$row['email'] .'</label>'."<label class='car_model'>".$row['model'] .'</label>' 
-            ."<label class='car_price'>".$row['price'] .'</label>'."<label class='reservation_date'>".$row['reserve_date'] 
-            .'</label>' .$delete."</div><br>";
+            $delete ="<br><input type='button' value='Delete' class='reservation_action' onclick='deleteReservation(event)'>";
+            $html_res .= "<div id=".$row['reservation_number']." class='reservation_div'>".
+                            "<img class='reservation_img' src='../car_images/".$row['plate_id'].".png'/>".
+                            "<div>".
+                                "<label class='reservation_label'>"."User Name" .'</label><br>'.
+                                "<label class='reservation_name reservation_label_value'>".$row['name'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Email" .'</label><br>'.
+                                "<label class='reservation_email reservation_label_value'>".$row['email'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."User ID" .'</label><br>'.
+                                "<label class='reservation_id reservation_label_value'>".$row['user_id'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Plate ID" .'</label><br>'.
+                                "<label class='reservation_plate_id reservation_label_value'>".$row['plate_id'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Year" .'</label><br>'.
+                                "<label class='reservation_year reservation_label_value'>".$row['year'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Model" .'</label><br>'.
+                                "<label class='reservation_model reservation_label_value'>".$row['model'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Price" .'</label><br>'.
+                                "<label class='reservation_price reservation_label_value'>".$row['price'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Return Date" .'</label><br>'.
+                                "<label class='reservation_return_date reservation_label_value'>".$row['return_date'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Pickup Date" .'</label><br>'.
+                                "<label class='reservation_pickup_date reservation_label_value'>".$row['pickup_date'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Reservation Date" .'</label><br>'.
+                                "<label class='reservation_date reservation_label_value'>".$row['reserve_date'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Reservation Number" .'</label><br>'.
+                                "<label class='reservation_number reservation_label_value'>".$row['reservation_number'] .'</label>'.
+                            "</div>".
+                $delete."</div><br>";
         }
         return $html_res;
     }
-    function show_reservations_customer($res){
+    function show_reservations($res){
         $html_res = "";
         while($row = mysqli_fetch_array($res)) {
-            $delete ="<br><input type='button' value='Delete' onclick='deleteReservation(event)'>";
-            $html_res .= "<div id=".$row['reservation_number']." class='reservation_div_customer'><label class='car_model'>".$row['model'] .'</label>' 
-            ."<label class='car_price'>".$row['price'] .'</label>'."<label class='reservation_date'>".$row['reserve_date'] 
-            .'</label>' .$delete."</div><br>";
+            $delete ="<br><input type='button' value='Delete' class='reservation_action' onclick='deleteReservation(event)'>";
+            $html_res .= "<div id=".$row['reservation_number']." class='reservation_div'>".
+                            "<img class='reservation_img' src='../car_images/".$row['plate_id'].".png'/>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Plate ID" .'</label><br>'.
+                                "<label class='reservation_plate_id reservation_label_value'>".$row['plate_id'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Year" .'</label><br>'.
+                                "<label class='reservation_year reservation_label_value'>".$row['year'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Model" .'</label><br>'.
+                                "<label class='reservation_model reservation_label_value'>".$row['model'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Price" .'</label><br>'.
+                                "<label class='reservation_price reservation_label_value'>".$row['price'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                            "<label class='reservation_label'>"."Return Date" .'</label><br>'.
+                            "<label class='reservation_return_date reservation_label_value'>".$row['return_date'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Pickup Date" .'</label><br>'.
+                                "<label class='reservation_pickup_date reservation_label_value'>".$row['pickup_date'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Reservation Date" .'</label><br>'.
+                                "<label class='reservation_date reservation_label_value'>".$row['reserve_date'] .'</label>'.
+                            "</div>".
+                            "<div>".
+                                "<label class='reservation_label'>"."Reservation Number" .'</label><br>'.
+                                "<label class='reservation_number reservation_label_value'>".$row['reservation_number'] .'</label>'.
+                            "</div>".
+                $delete."</div><br>";
         }
         return $html_res;
     }
