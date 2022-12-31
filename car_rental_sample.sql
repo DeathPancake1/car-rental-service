6bb41a25556701e430eaf5bf21b25ebc7e200889
DeathPancake1 <youssefyoyododo@gmail.com>
2022-12-31 04:23:10 +0200

    Update car_rental_sample.sql

--- car_rental_sample.sql
+++ car_rental_sample.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2022 at 10:53 PM
+-- Generation Time: Dec 31, 2022 at 03:22 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -60,9 +60,15 @@ CREATE TABLE `car` (
 
 INSERT INTO `car` (`plate_id`, `model`, `year`, `price`) VALUES
 ('1', 'C class', 2019, 500),
+('10', 'fasdkjhl', 1980, 700),
 ('2', 'Matrix', 2016, 200),
 ('3', 'Verna', 2008, 400),
-('4', 'Seat 133', 1980, 100);
+('4', 'Seat 133', 1980, 100),
+('5', 'fasdkjhl', 1980, 700),
+('6', 'fasdkjhl', 1980, 700),
+('7', 'fasdkjhl', 1980, 700),
+('8', 'fasdkjhl', 1980, 700),
+('9', 'fasdkjhl', 1980, 700);
 
 -- --------------------------------------------------------
 
@@ -83,15 +89,19 @@ CREATE TABLE `car_status` (
 
 INSERT INTO `car_status` (`plate_id`, `today`, `status`, `reserved`) VALUES
 ('1', '2022-12-28 23:37:02', 'active', 'NO'),
-('2', '2022-12-27 00:00:00', 'active', 'YES'),
+('1', '2022-12-29 00:00:00', 'active', 'YES'),
+('1', '2022-12-30 00:00:00', 'active', 'NO'),
+('10', '2022-12-31 03:04:51', 'active', 'NO'),
 ('2', '2022-12-28 23:37:28', 'active', 'NO'),
-('2', '2022-12-30 00:00:00', 'active', 'NO'),
-('2', '2022-12-31 00:00:00', 'active', 'NO'),
 ('3', '2022-12-28 23:37:48', 'active', 'NO'),
-('4', '2022-12-27 00:00:00', 'active', 'YES'),
 ('4', '2022-12-28 23:38:07', 'out of service', 'NO'),
-('4', '2022-12-28 23:46:56', 'active', 'NO'),
-('4', '2022-12-30 00:00:00', 'active', 'NO');
+('5', '2022-12-27 00:00:00', 'active', 'YES'),
+('5', '2022-12-30 00:00:00', 'active', 'NO'),
+('5', '2022-12-31 03:04:27', 'active', 'NO'),
+('6', '2022-12-31 03:04:38', 'active', 'NO'),
+('7', '2022-12-31 03:04:41', 'active', 'NO'),
+('8', '2022-12-31 03:04:44', 'active', 'NO'),
+('9', '2022-12-31 03:04:48', 'active', 'NO');
 
 -- --------------------------------------------------------
 
@@ -107,6 +117,13 @@ CREATE TABLE `payment` (
   `method` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`reservation_number`, `user_id`, `payment_time`, `amount`, `method`) VALUES
+(7, 2, '2022-12-31 03:30:38', '500', '');
+
 -- --------------------------------------------------------
 
 --
@@ -114,7 +131,7 @@ CREATE TABLE `payment` (
 --
 
 CREATE TABLE `reservation` (
-  `reserve_date` datetime DEFAULT NULL,
+  `reserve_date` datetime NOT NULL,
   `user_id` int(11) NOT NULL,
   `reservation_number` int(11) NOT NULL,
   `plate_id` varchar(50) NOT NULL
@@ -125,8 +142,8 @@ CREATE TABLE `reservation` (
 --
 
 INSERT INTO `reservation` (`reserve_date`, `user_id`, `reservation_number`, `plate_id`) VALUES
-('2022-12-28 23:49:04', 2, 2, '2'),
-('2022-12-28 23:53:04', 3, 3, '4');
+('2022-12-31 03:00:59', 2, 7, '1'),
+('2022-12-31 03:51:50', 2, 17, '5');
 
 -- --------------------------------------------------------
 
@@ -146,8 +163,8 @@ CREATE TABLE `reserve_status` (
 --
 
 INSERT INTO `reserve_status` (`plate_id`, `reservation_number`, `pickup_date`, `return_date`) VALUES
-('2', 2, '2022-12-27 00:00:00', '2022-12-30 00:00:00'),
-('4', 3, '2022-12-27 00:00:00', '2022-12-30 00:00:00');
+('1', 7, '2022-12-29 00:00:00', '2022-12-30 00:00:00'),
+('5', 17, '2022-12-27 00:00:00', '2022-12-30 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -198,14 +215,13 @@ ALTER TABLE `car_status`
 -- Indexes for table `payment`
 --
 ALTER TABLE `payment`
-  ADD PRIMARY KEY (`reservation_number`,`user_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`reservation_number`,`user_id`);
 
 --
 -- Indexes for table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`user_id`,`plate_id`),
+  ADD PRIMARY KEY (`user_id`,`plate_id`,`reserve_date`),
   ADD UNIQUE KEY `reservation_number` (`reservation_number`),
   ADD KEY `plate_id` (`plate_id`);
 
@@ -232,13 +248,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `reservation_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `reservation_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `reservation_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `reservation_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -260,8 +276,7 @@ ALTER TABLE `car_status`
 -- Constraints for table `payment`
 --
 ALTER TABLE `payment`
-  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`reservation_number`) REFERENCES `reservation` (`reservation_number`) ON DELETE CASCADE,
-  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `reservation` (`user_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`reservation_number`,`user_id`) REFERENCES `reservation` (`reservation_number`, `user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `reservation`
